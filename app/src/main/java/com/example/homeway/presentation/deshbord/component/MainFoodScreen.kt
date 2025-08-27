95c713330c24a107b3152a6b1a75a58965277508
HOMEWAY Group <mohitprajapatisingh2002@gmail.com>
2025-08-27 14:45:08 +0530

    my first android project

--- app/src/main/java/com/example/homeway/presentation/deshbord/component/MainFoodScreen.kt
+++ app/src/main/java/com/example/homeway/presentation/deshbord/component/MainFoodScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -17,12 +18,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.WifiCalling3
@@ -31,9 +38,19 @@ import androidx.compose.material.icons.outlined.Reviews
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -51,9 +68,17 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.homeway.R
 
-@Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FoodScreen() {
+fun FoodScreen(categories: List<String>,
+               selectedTabIndex: Int,
+               onTabSelected: (Int) -> Unit) {
+
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = false,
+    )
 
      val MergFont = FontFamily(
         Font(R.font.mergeone_regular, FontWeight.Normal)
@@ -85,6 +110,34 @@ fun FoodScreen() {
                         contentScale = ContentScale.Crop,
                         modifier = Modifier.fillMaxSize()
                     )
+
+
+                    Image(
+                        painter = painterResource(R.drawable.veg_icon),
+                        contentDescription = "veg",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .size(30.dp)
+                            .align(Alignment.BottomEnd)
+                            .padding(end = 12.dp, bottom = 12.dp)
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .size(30.dp)
+                            .clip(CircleShape)
+                            .background(Color.Black.copy(alpha = 0.4f))
+                    ){
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier.padding(6.dp)
+                        )
+                    }
+
+
                 }
                 Column (
                     modifier = Modifier
@@ -93,13 +146,13 @@ fun FoodScreen() {
                     verticalArrangement = Arrangement.spacedBy(5.dp)
                 ){
                     Row (
-                        modifier = Modifier,
-                        horizontalArrangement = Arrangement.spacedBy(160.dp),
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.CenterVertically
                     ){
                         Text(
                             text = "Penne Pasta",
-                            fontSize = 32.sp,
+                            fontSize = 29.sp,
                             fontWeight = FontWeight.Normal,
                             fontFamily = MergFont,
                             color = Color(0xFF2B2B2B)
@@ -132,18 +185,33 @@ fun FoodScreen() {
                                     imageVector = Icons.Default.Star,
                                     contentDescription = null,
                                     tint = Color(0xFFFFFFFF),
-                                    modifier = Modifier.size(7.dp)
+                                    modifier = Modifier.size(9.dp)
                                 )
                             }
                         }
                     }
 
-                    Text(
-                        text = "Originates , Italy",
-                        fontSize = 15.sp,
-                        fontFamily =mac,
-                        fontWeight = FontWeight.Normal
-                    )
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Originates , Italy",
+                            fontSize = 15.sp,
+                            fontFamily = mac,
+                            fontWeight = FontWeight.Normal
+                        )
+
+                        Text(
+                            text = "Today 10:00Am to 11:00Pm",
+                            fontSize = 10.sp,
+                            fontWeight = FontWeight.Normal,
+                            fontFamily = MergFont,
+                            color = Color(0xFF2B2B2B)
+                        )
+
+                    }
 
                     Row (
                         modifier = Modifier.fillMaxWidth(),
@@ -157,37 +225,78 @@ fun FoodScreen() {
                             fontFamily = mac,
                             color = Color(0xFF2B2B2B)
                         )
+                        Card (
+                            modifier = Modifier
+                                .width(33.dp)
+                                .height(12.dp)
+                                .align(Alignment.CenterVertically),
+                            colors = CardDefaults.cardColors(containerColor = Color(0xFFE73F46)),
+                            shape = RoundedCornerShape(5.dp)
+                        ){
+                            Box(
+                                modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(),
+                                contentAlignment = Alignment.Center) {
+                                Text(
+                                    text = "OPEN",
+                                    fontSize = 9.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    fontFamily = MergFont,
+                                    color = Color(0xFFFFFFFF)
+                                )
+                            }
+                        }
 
-                        Text(
-                            text = "1.6 Km",
-                            fontSize = 12.sp,
-                            fontWeight = FontWeight.Normal,
-                            fontFamily = mac,
-                            color = Color(0xFF2B2B2B)
-                        )
                     }
-                    Row (
-                        modifier = Modifier.height(20.dp).width(115.dp),
+                    Row(
+                        modifier = Modifier
+                            .height(20.dp)
+                            .fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.CenterVertically
-                    ){
-                        Icon(
-                            imageVector = Icons.Default.WifiCalling3,
-                            contentDescription = null,
-                            tint = Color(0xFF6471FF),
-                            modifier = Modifier.size(17.dp)
-                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .height(20.dp)
+                                .width(194.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.WifiCalling3,
+                                contentDescription = null,
+                                tint = Color(0xFF6471FF),
+                                modifier = Modifier.size(17.dp)
+                            )
 
+                            Text(
+                                text = "+916263672804",
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Normal,
+                                fontFamily = MergFont,
+                                color = Color(0xFF4B4B4B)
+                            )
+
+                            VerticalDivider(thickness = 0.8.dp, modifier = Modifier.height(17.dp), color = Color.Gray)
+
+                            Text(
+                                text = "₹250 for one",
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Normal,
+                                fontFamily = MergFont,
+                                color = Color(0xFF4B4B4B)
+                            )
+
+                        }
                         Text(
-                            text = "+916263672804",
+                            text = "Distance 1.6 Km",
                             fontSize = 12.sp,
                             fontWeight = FontWeight.Normal,
                             fontFamily = MergFont,
-                            color = Color(0xFF4B4B4B)
+                            color = Color(0xFF2B2B2B)
                         )
                     }
-                    Spacer(modifier = Modifier.height(15.dp))
-
 
                     Row (
                         modifier = Modifier
@@ -202,9 +311,9 @@ fun FoodScreen() {
                         OutlinedCard(
                             border = BorderStroke(1.dp, Color.Black.copy(alpha = 0.7f)),
                             modifier = Modifier
-                                .clickable{}
+                                .clickable {}
                                 .width(88.dp)
-                                .height(32.dp)
+                                .height(28.dp)
                                 .align(Alignment.CenterVertically),
                             colors = CardDefaults.cardColors(containerColor = Color.White),
                             shape = RoundedCornerShape(6.dp)
@@ -212,7 +321,7 @@ fun FoodScreen() {
                             Row(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .height(32.dp)
+                                    .height(28.dp)
                             ) {
 
                                 Icon(
@@ -246,9 +355,9 @@ fun FoodScreen() {
                         OutlinedCard(
                             border = BorderStroke(1.dp, Color.Black.copy(alpha = 0.7f)),
                             modifier = Modifier
-                                .clickable{}
+                                .clickable {}
                                 .width(76.dp)
-                                .height(32.dp)
+                                .height(28.dp)
                                 .align(Alignment.CenterVertically),
                             colors = CardDefaults.cardColors(containerColor = Color.White),
                             shape = RoundedCornerShape(6.dp)
@@ -256,7 +365,7 @@ fun FoodScreen() {
                             Row(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .height(32.dp)
+                                    .height(28.dp)
                             ) {
 
                                 Icon(
@@ -290,9 +399,9 @@ fun FoodScreen() {
                         OutlinedCard(
                             border = BorderStroke(1.dp, Color.Black.copy(alpha = 0.7f)),
                             modifier = Modifier
-                                .clickable{}
+                                .clickable {}
                                 .width(80.dp)
-                                .height(32.dp)
+                                .height(28.dp)
                                 .align(Alignment.CenterVertically),
                             colors = CardDefaults.cardColors(containerColor = Color.White),
                             shape = RoundedCornerShape(6.dp)
@@ -300,7 +409,7 @@ fun FoodScreen() {
                             Row(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .height(32.dp)
+                                    .height(28.dp)
                             ) {
 
                                 Icon(
@@ -335,7 +444,7 @@ fun FoodScreen() {
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .height(250.dp)
+                            .height(190.dp)
                             .padding(8.dp)
                     ) {
                         // Left Grid - 4 small images
@@ -419,6 +528,69 @@ fun FoodScreen() {
                         }
                     }
 
+                    Box(modifier = Modifier
+                        .fillMaxWidth(),contentAlignment = Alignment.Center) {
+                        ScrollableTabRow(
+                            selectedTabIndex = selectedTabIndex,
+                            backgroundColor = Color.White,
+                            edgePadding = 3.dp,
+                            indicator = { tabPositions ->
+                                TabRowDefaults.Indicator(
+                                    modifier = Modifier
+                                        .tabIndicatorOffset(tabPositions[selectedTabIndex])
+                                        .height(3.dp),
+                                    color = Color(0xFF7A63FF),
+
+                                    )
+                            },
+                            divider = {}
+                        ) {
+                            categories.forEachIndexed { index, category ->
+                                Tab(
+                                    selected = selectedTabIndex == index,
+                                    onClick = { onTabSelected(index) },
+                                    selectedContentColor = Color.Black,
+                                    unselectedContentColor = Color.Black.copy(alpha = 2.8f),
+                                    modifier = Modifier.width(IntrinsicSize.Min)
+
+                                ) {
+                                    Row(
+                                        horizontalArrangement = Arrangement.Center,
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        modifier = Modifier.fillMaxWidth().padding(horizontal = 6.dp, vertical = 8.dp)
+                                    ) {
+                                        Text(
+                                            text = category,
+                                            fontSize = 15.sp,
+                                            fontWeight = if (selectedTabIndex == index) FontWeight.Bold else FontWeight.Normal,
+                                            fontFamily = MergFont,
+                                            modifier = Modifier.padding(end = 20.dp)
+                                        )
+
+                                        if (category == "See all") {
+                                            Icon(
+                                                imageVector = Icons.Default.KeyboardArrowDown,
+                                                contentDescription = "Arrow",
+                                                tint = Color(0xFF6471FF),
+                                                modifier = Modifier
+                                                    .size(18.dp)
+                                                    .clickable { showBottomSheet = true }
+                                            )
+                                            if (showBottomSheet) {
+                                                ModalBottomSheet(
+                                                    modifier = Modifier.fillMaxHeight(),
+                                                    sheetState = sheetState,
+                                                    onDismissRequest = { showBottomSheet = false }
+                                                ) {
+                                                    SEE_ALL()
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
 
                 }
 
@@ -428,4 +600,17 @@ fun FoodScreen() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun ok() {
+    var selectedTab by remember { mutableStateOf(0) }
+    val categories = listOf(
+        "Overview", "Online","Review","Menu",
+
+        )
+    FoodScreen(categories = categories,
+        selectedTabIndex = selectedTab,
+        onTabSelected = { selectedTab = it })
 }
